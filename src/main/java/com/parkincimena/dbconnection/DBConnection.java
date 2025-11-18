e69b17a89b08ac2dc1fd0a52a49877e22d9840ba
Snow-124 <kryster_limpin@dlsu.edu.ph>
2025-11-18 18:01:19 +0800

    Connection Fix - Java MySQL

--- src/main/java/com/parkincimena/dbconnection/DBConnection.java
+++ src/main/java/com/parkincimena/dbconnection/DBConnection.java
@@ -1,5 +1,20 @@
+package com.parkincimena.dbconnection;
 
+import java.sql.Connection;
+import java.sql.DriverManager;
+import java.sql.SQLException;
 
 public class DBConnection {
+
+    // database name
+    private static final String URL = "jdbc:mysql://localhost:3306/parkin_db"; // named the schema as "parkin_db", just replace if you have your own
+
+    // database credentials
+    private static final String USER = "root";
+    private static final String PASSWORD = "admin"; // just replace with your own password if you want to test
+
+    public static Connection getConnection() throws SQLException {
+        return DriverManager.getConnection(URL, USER, PASSWORD);
+    }
     
 }
